f7002dc77582491428811f1250b28d479f3fe523
Yoyoyohanne <johannemargrethelund@hotmail.dk>
2024-11-26 14:17:27 +0100

    tpm

--- 05_correlation_matrix.R
+++ 05_correlation_matrix.R
@@ -11,8 +11,8 @@ library(reshape2)
 
 # Check if the variables already exists, otherwise read the file
 
-if (!exists("cancer_count")) {
-  cancer_count <- read.delim("_raw/TCGA-PAAD.star_counts.tsv", header = TRUE, sep = "\t", row.names = 1)
+if (!exists("cancer_tpm")) {
+  cancer_tpm <- read.delim("_raw/TCGA-PAAD.star_tpm.tsv", header = TRUE, sep = "\t", row.names = 1)
 }
 
 if (!exists("membrane_data")) {
@@ -20,36 +20,33 @@ if (!exists("membrane_data")) {
 }
 
 if (!exists("filterede_data")) {
-  filtered_data<-read_csv('filtered_deseq2_results.csv')
+  filtered_data<-read_csv('data/filtered_deseq2_results.csv')
 }
 
 
-
-
 # Format data
-rownames(cancer_count) <- gsub("\\.\\d+$", "", rownames(cancer_count))
+rownames(cancer_tpm) <- gsub("\\.\\d+$", "", rownames(cancer_tpm))
 
 # Match TCGA & Deeploc data
-data <- cancer_count[rownames(cancer_count) %in% membrane_data$To,]
+data <- cancer_tpm[rownames(cancer_tpm) %in% membrane_data$To,]
 
 # Match TCGA & DESeq2 data
-
 gene_list <- filtered_data$...1
 filtered_counts <- data[row.names(data) %in% gene_list, ]
 
 
-#transform to fit correllation:
+# Transform to fit correllation:
 filtered_counts <- t(filtered_counts)
 
 # Getting correlation matrix:
-cor_filtered_counts <- cor(filtered_counts, method = "spearman")
+cor_filtered_counts <- cor(filtered_counts, method = "pearson")
 
 
-#getting the data in the right format:
+# Getting the data in the right format:
 melt_data<-melt(cor_filtered_counts)
 
 
-# Create a ggheatmap
+# Creating a ggheatmap
 ggheatmap <- ggplot(melt_data, aes(Var2, Var1, fill = value))+
   geom_tile(color = "white")+
   scale_fill_gradient2(low = "blue", high = "red", mid = "white", 
@@ -63,7 +60,11 @@ ggheatmap <- ggplot(melt_data, aes(Var2, Var1, fill = value))+
 # Print the heatmap
 print(ggheatmap)
 
+#remove correlation with self and NA:
+clean_matrix <- melt_data[complete.cases(melt_data), ]
 
+#Getting the top correlated gene_pairs:
+top_corr_data <- melt_data[melt_data[, 3] > 0.95, ] |> melt_data[melt_data[, 3] != 1, ]
 
 
 
